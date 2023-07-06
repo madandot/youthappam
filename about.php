ff5543efd9c3f8e2651e38d75335194aa9f5c677
madan <reddymadan0512#gmail.com>
2023-07-06 13:12:56 +0530

    update

---
+++ about.php
@@ -0,0 +1,47 @@
+<?php include('./constant/layout/head.php');?>
+<?php include('./constant/layout/header.php');?>
+
+<?php include('./constant/layout/sidebar.php');?>
+
+
+<div class="page-wrapper">
+	<div class="container-fluid">
+		<div class="card">
+			<div class="card-body">
+				<div class="about-head text-center mb-5">
+					<h1 style="color: black;" class="mb-2 mt-4 f-s-50 ">About Nagendra.</h1>
+					<span>Freelancer Web Developer</span>
+				</div>
+				<div class="about">
+					<div class="row">
+						<div class="col-md-3">
+							<img src="assets/myimages/profile.jpg" class="img-fluid">
+						</div>
+						<!--  Author : Nagendra. 
+ for any PHP, Codeignitor, Laravel OR Python work contact me at email mayuri.infospace@gmail.com  
+ Visit website - www.mayurik.com -->
+						<div class="col-md-5">
+							<h3 class="mb-1" style="text-align: justify; color: gray;">A Computer Science master graduate, keen on stirring up creative mind and information into PHP project ideas and working as a freelancer for PHP projects with source code. She also work in Python, Codeignitor and Laravel. Nagendra likes to learn new things related to her profession.</h3>
+							<h3 style="text-align: justify; color: gray;" >Soon she is going to launch an online course on PHP projects for beginners. You can contact her for any Project development, either academic or commercial Projects.</h3>
+							<h3 style="text-align: justify;"><b><mark>For students or anyone else who needs program or source code for thesis writing or any Professional Software Development, Website Development, at affordable cost contact at mayuri.infospace@gmail.com</mark></b></h3>
+						</div>
+						<div class="col-md-4">
+							<ul class="list-unstyled">
+								<><span>Expert in</span> : <b>HTML, PHP, MySQL</
+								<li><span>Residence</span> :  <b>India</b></li>
+								<li><span>Email</span> :  <b>mayuri.infospace@gmail.com</b></li>
+							</ul>
+						</div>
+					</div>
+					
+				</div>
+			</div>			
+		</div>
+	</div>
+</div>
+
+
+<?php include('./constant/layout/footer.php');?>
+<!--  Author Name- Nagendra. 
+ for any PHP, Codeignitor, Laravel OR Python work contact me at mayuri.infospace@gmail.com  
+ Visit website - www.mayurik.com -->
